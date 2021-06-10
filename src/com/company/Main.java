33c60b2cc8ec7291c5d2e7e0cc9d14a808b344be
YSilahin <egor.silagin96@gmail.com>
2021-06-10 18:11:16 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String st = "The quick brown fox jumps over the lazy dog.";
+
+        String new_st = st.replace('d', 'f');
+
+        System.out.println("Original string: " + st);
+        System.out.println("New String: " + new_st);
+
+    }
+}
