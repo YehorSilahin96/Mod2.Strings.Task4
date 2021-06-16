56acdfec9fbfdb1be9de87af373026f183ef35ca
AnnaDremluga <dreanna3@gmail.com>
2021-06-16 23:00:54 +0300

    PR review

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
         String st = "The quick brown fox jumps over the lazy dog.";
 
-        String new_st = st.replace('d', 'f');
-
+        String new_st = st.replace('d', 'f'); // do not use "_" in naming
+        // what if the string is "The stupid yellow duck every day jumps over the lazy dog."? Please, implement it. (Always think about universal solutions)
         System.out.println("Original string: " + st);
         System.out.println("New String: " + new_st);
 
